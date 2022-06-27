87d1d5be4b359c1ef8389489a448ee52dd230fd0
John Paulo A. Geronimo <77067773+geronimium-gr@users.noreply.github.com>
2022-06-27 23:42:25 +0800

    component: About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import sample from "../images/img-4.jpg";
 
 const About = () => {
   return (
-    <section id="about" className="pt-15">
+    <section id="about">
       <div className="bg-whitish shadow-md overflow-hidden">
         <div className="md:flex">
           <div className="md:shrink-0">
@@ -13,23 +13,21 @@ const About = () => {
               alt="Man looking at item at a store"
             />
           </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              park jiwon
+          <div className="flex flex-col md:flex-row">
+            <div className="p-10 md:p-20">
+              <p className="mt-2 text-slate-500 max-w-md text-2xl text-center font-bold text-whitish md:text-3xl">
+                About Me
+              </p>
             </div>
-            <a
-              href="https://"
-              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-            >
-              Fromis 9
-            </a>
-            <p className="mt-2 text-slate-500">
-              Park Jiwon (박지원) was born on March 20, 1998 in Busan, South
-              Korea. She was a trainee for JYP Entertainment from 2012-2015,
-              leaving shortly after being eliminated from the survival show
-              SIXTEEN. In 2017 she became a contestant on the survival show Idol
-              School and placed sixth, making her a member of Fromis_9.
-            </p>
+            <div className="p-8 place-self-center">
+              <p className="mt-2 text-slate-500">
+                Park Jiwon (박지원) was born on March 20, 1998 in Busan, South
+                Korea. She was a trainee for JYP Entertainment from 2012-2015,
+                leaving shortly after being eliminated from the survival show
+                SIXTEEN. In 2017 she became a contestant on the survival show
+                Idol School and placed sixth, making her a member of Fromis_9.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <section id="hero">
+    <section id="hero" className="h-screen">
       <div className="container flex flex-col-reverse item-center px-6 mx-auto mt-10 space-y-0 md:flex-row md:space-y-0">
         <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
           <h1 className="text-whitish max-w-md text-4xl font-bold text-center md:text-6xl md:text-left">
